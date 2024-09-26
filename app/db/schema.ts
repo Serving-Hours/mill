e8299649c751ddab00edcdcc6f6dba860e827775
frija <lucydabestt@gmail.com>
2024-09-26 17:37:27 +0800

    (feat): Configure database

---
+++ app/db/schema.ts
@@ -0,0 +1,10 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const linksTable = sqliteTable("links", {
+  id: integer("id").primaryKey(),
+  url: text("url").notNull(),
+  slug: text("slug").unique().notNull()
+});
+
+export type InsertLink = typeof linksTable.$inferInsert;
+export type SelectLink = typeof linksTable.$inferSelect;
