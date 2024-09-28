8cc6a38195b79dbf75c10ad3a223b57b705e5ee2
frija <lucydabestt@gmail.com>
2024-09-28 19:38:17 +0800

    (update): Small changes to auth flow

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Login() {
+  const { status } = useSession();
+
+  if (status === "authenticated") {
+    return redirect("/links");
+  }
+
   return (
     <main className="grid place-items-center">
       <div>Hello</div>
       <button onClick={() => signIn("google", { callbackUrl: "/links" })}>Continue with Google</button>
-      <button onClick={() => signIn("github" , {callbackUrl:"/links"})}>Continue with github</button>
-      <button onClick={() => signOut()}>Signout </button>
+      <button onClick={() => signIn("github", { callbackUrl: "/links" })}>Continue with github</button>
     </main>
   );
 }
