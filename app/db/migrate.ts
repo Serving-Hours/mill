7ee78255ee2103145dafa95f71127c60b9a87939
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-09-28 18:08:39 +0800

    feat: add migrations functionality

---
+++ app/db/migrate.ts
@@ -0,0 +1,12 @@
+import { db } from '.';
+import { migrate } from 'drizzle-orm/libsql/migrator';
+import path from 'path';
+
+(async () => {
+  console.log("Migrations Running");
+
+  const migrationsPath = path.resolve(__dirname, '../../migrations');
+  await migrate(db, { migrationsFolder: migrationsPath });
+
+  console.log("migrations success");
+})();
