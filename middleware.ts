9b539cc5442871595fd457404da6be9aa9060eb9
frija <lucydabestt@gmail.com>
2024-10-08 22:14:59 +0800

    (update): Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,28 @@
 import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
-  const isAuthenticated = !!token;
+export default withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuth = !!token;
 
-  if (req.nextUrl.pathname.startsWith("/dashboard") && !isAuthenticated) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+    if (!isAuth) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true;
+      },
+    },
+  },
+);
 
-  return NextResponse.next();
-}
+export const config = {
+  matcher: ["/dashboard/:path*", "/link/:path*"],
+};
